731e208609400feb56e1a76cfe29a4b080ab4619
Srejon Joy <89932669+SrejonJoy@users.noreply.github.com>
2024-07-17 21:16:56 +0600

    Update about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -6,7 +6,7 @@ const AboutPage = () => {
                 Welcome to Eschool, your gateway to limitless learning opportunities in the digital age. At Eschool, we believe in transforming education through technology, empowering students and educators alike to thrive in an immersive online learning environment.
             </p>
             <p>
-                Eschool offers a diverse range of courses designed to cater to every learner's unique journey. Whether you're a student seeking academic enrichment or a professional looking to advance your career, our platform provides access to expertly crafted curriculum and interactive learning tools that foster growth and success.
+                Eschool offers a diverse range of courses designed to cater to every learner&apos;s unique journey. Whether you&apos;re a student seeking academic enrichment or a professional looking to advance your career, our platform provides access to expertly crafted curriculum and interactive learning tools that foster growth and success.
             </p>
             <p>
                 With a commitment to excellence, Eschool combines cutting-edge technology with a passion for education, ensuring a seamless and enriching experience for all users. Join us as we redefine learning in the 21st century and embark on a journey towards knowledge, innovation, and personal achievement. Discover your potential with Eschool - where education meets innovation, and the future of learning begins today
@@ -14,3 +14,5 @@ const AboutPage = () => {
         </div>
     );
 };
+
+export default AboutPage;
